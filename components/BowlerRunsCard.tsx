3204a76d00b1ae06044e8ae04707d33eeda31fa2
priyanshu334 <99priyanshu33@gmail.com>
2025-04-23 19:17:32 +0530

    oo

--- components/BowlerRunsCard.tsx
+++ components/BowlerRunsCard.tsx
@@ -55,7 +55,7 @@ const BowlerRunsCard: React.FC<BowlerRunsCardProps> = ({
 
     try {
       toast.promise(
-        fetch("/api/bowlerruns/place", {
+        fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/bowlerruns/place`, {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
